5e86963bdbe69d5d4d14b5099aa09afab5b4fde0
Pavlo Kikh <pavlokiht2005@gmail.com>
2025-08-04 19:34:24 +0200

    Add expiration feature to AdvertisementMaterial
    - Introduced 'expires_at' field to track expiration date.
    - Added 'never_expires' checkbox to the form for indefinite materials.
    - Implemented logic to handle expiration in the form and model.
    - Created management command to clean up expired materials.
    - Updated templates to display expiration information.

--- advertisements/forms.py
+++ advertisements/forms.py
@@ -1,39 +1,54 @@
 from django import forms
 from .models import Store, Department, Stand, AdvertisementMaterial
+import datetime
 
 class AdvertisementMaterialForm(forms.ModelForm):
+    never_expires = forms.BooleanField(
+        label="Bezterminowy",
+        required=False,
+        initial=True,
+        help_text="Zaznacz, jeśli materiał nie powinien automatycznie wygasać"
+    )
+    
     class Meta:
         model = AdvertisementMaterial
-        fields = ['stand', 'material_type', 'file', 'status', 'duration']
+        fields = ['stand', 'material_type', 'file', 'status', 'duration', 'expires_at']
         widgets = {
             'stand': forms.Select(attrs={'class': 'form-select'}),
             'material_type': forms.Select(attrs={'class': 'form-select'}),
             'file': forms.FileInput(attrs={'class': 'form-control'}),
             'status': forms.Select(attrs={'class': 'form-select'}),
             'duration': forms.NumberInput(attrs={'class': 'form-control', 'min': 1}),
+            'expires_at': forms.DateTimeInput(
+                attrs={'class': 'form-control', 'type': 'datetime-local'}, 
+                format='%Y-%m-%dT%H:%M'
+            ),
         }
     
     def __init__(self, *args, **kwargs):
-        user = kwargs.pop('user', None)
         super().__init__(*args, **kwargs)
         
-        # Jeżeli użytkownik jest przekazany, ogranicz wybór stanowiska
-        if user:
-            if user.is_superadmin():
-                # Superadmin może wybierać wszystkie stanowiska
-                pass
-            elif user.is_store_admin() and user.managed_store:
-                # Admin sklepu może wybierać tylko stanowiska w swoim sklepie
-                departments = user.managed_store.departments.all()
-                stands = Stand.objects.filter(department__in=departments)
-                self.fields['stand'].queryset = stands
-            elif user.is_editor() and user.managed_stand:
-                # Edytor może wybierać tylko swoje stanowisko
-                self.fields['stand'].queryset = Stand.objects.filter(id=user.managed_stand.id)
-                self.fields['stand'].widget.attrs['disabled'] = 'disabled'
-                self.fields['stand'].widget.attrs['readonly'] = True
-                if not self.initial.get('stand'):
-                    self.initial['stand'] = user.managed_stand
+        # Ukryj pole expires_at początkowo, jeśli jest nowy materiał lub ma nieokreślony termin
+        if self.instance.pk is None or self.instance.expires_at is None:
+            self.fields['never_expires'].initial = True
+        else:
+            self.fields['never_expires'].initial = False
+            
+        # Ustaw minimalną datę na jutro
+        tomorrow = datetime.datetime.now()
+        self.fields['expires_at'].widget.attrs['min'] = tomorrow.strftime('%Y-%m-%dT%H:%M')
+        
+    def clean(self):
+        cleaned_data = super().clean()
+        never_expires = cleaned_data.get('never_expires')
+        expires_at = cleaned_data.get('expires_at')
+        
+        if never_expires:
+            cleaned_data['expires_at'] = None
+        elif not expires_at:
+            self.add_error('expires_at', 'Wybierz datę wygaśnięcia lub zaznacz "Bezterminowy"')
+            
+        return cleaned_data
 
 
 class StandAnimationForm(forms.ModelForm):
